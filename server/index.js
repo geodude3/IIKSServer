b5216c85b9dc49dc69a11be9985b3367fcd4bbf2
geodude3 <77406516+geodude3@users.noreply.github.com>
2023-01-09 09:03:34 -0800

    first commit

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { appendFile } = require('fs');
+const {readFile, readFileSync, read} = require('fs');
+const app = express();
+
+var cors = require('cors')
+
+app.use(cors()) // Use this after the variable declaration
+
+let connections = 1;
+
+/*
+app.get("/connections", (req, res) => {
+    res.json({ connections: connections});
+});
+*/
+
+
+app.get("/api/connections", (req, res) => {
+    res.json({ message: connections });
+    connections++;
+});
+
+
+app.get("/api", (request, response) => {
+    response.json({ message: "Hi from server" });
+    connections++;
+
+});
+
+
+
+app.listen(process.env.PORT || 3001, ()=>console.log("server launched on port 3001"));
